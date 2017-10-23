4e3b80d2d979a9bda0bd438c81db0a7e5589d9b8
Soner Tari <sonertari@gmail.com>
2017-10-23 23:06:14 +0300

    Update translations for 6.2

--- src/create_po.sh
+++ src/create_po.sh
@@ -82,7 +82,7 @@ if ! xgettext -L "PHP" -s \
 		--copyright-holder="Soner Tari, The UTMFW project" \
 		--msgid-bugs-address="sonertari@gmail.com" \
 		--package-name="UTMFW" \
-		--package-version="5.9" \
+		--package-version="6.2" \
 		-j -o $LOCALE_FILE \
 		-f files.txt; then
 	echo "FAILED generating $LOCALE_FILE"
