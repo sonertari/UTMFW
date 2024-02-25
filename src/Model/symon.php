541a4308a50f30222af2029bebcd799ca5b64652
Soner Tari <sonertari@gmail.com>
2024-02-25 18:09:41 +0300

    Get number of cpus from hw.ncpuonline not from hw.ncpu
    On OpenBSD hyperthreading is disabled, so while ncpu reports 8 cores,
    ncpuonline is 4.
    And since the installer configures symon cpu layout with 8 cpus,
    class_graph.inc errors out with "graph: internal error : need to save
    the graph before an url can be requested" on System>Graphs>CPUs.

--- src/Model/symon.php
+++ src/Model/symon.php
@@ -118,8 +118,8 @@ class Symon extends Monitoring
 	{
 		/// @todo See the new OpenBSD installer to get this and sensors paths
 		$ncpu= 1;
-		if (($hwncpu= $this->_getSysCtl('hw.ncpu')) !== FALSE) {
-			if (preg_match('|^hw.ncpu=(\d+)$|ms', $hwncpu, $match)) {
+		if (($hwncpu= $this->_getSysCtl('hw.ncpuonline')) !== FALSE) {
+			if (preg_match('|^hw.ncpuonline=(\d+)$|ms', $hwncpu, $match)) {
 				$ncpu= $match[1];
 			}
 		}
@@ -270,8 +270,8 @@ class Symon extends Monitoring
 	function SetCpus()
 	{
 		$ncpu= 1;
-		if (($hwncpu= $this->_getSysCtl('hw.ncpu')) !== FALSE) {
-			if (preg_match('|^hw.ncpu=(\d+)$|ms', $hwncpu, $match)) {
+		if (($hwncpu= $this->_getSysCtl('hw.ncpuonline')) !== FALSE) {
+			if (preg_match('|^hw.ncpuonline=(\d+)$|ms', $hwncpu, $match)) {
 				$ncpu= $match[1];
 			}
 		}
